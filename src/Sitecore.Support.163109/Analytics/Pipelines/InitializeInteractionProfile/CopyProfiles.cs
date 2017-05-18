f417e774e7f57178e42263c2bfc74f63fa4e2254
DK\mdo <mdo@sitecore.net>
2017-05-18 16:43:33 +0300

    Commit patch

---
+++ src/Sitecore.Support.163109/Analytics/Pipelines/InitializeInteractionProfile/CopyProfiles.cs
@@ -0,0 +1,49 @@
+﻿using Sitecore.Analytics.Data;
+using Sitecore.Analytics.Model;
+using Sitecore.Analytics.Tracking;
+using Sitecore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using Sitecore.Analytics.Pipelines.InitializeInteractionProfile;
+
+namespace Sitecore.Support.Analytics.Pipelines.InitializeInteractionProfile
+{
+    public class CopyProfiles : InitializeInteractionProfileProcessor
+    {
+        public override void Process(InitializeInteractionProfileArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+            Assert.IsNotNull(args.Session, "args.Session");
+            Contact contact = args.Session.Contact;
+            CurrentInteraction interaction = args.Session.Interaction;
+            if (contact != null && interaction != null)
+            {
+                BehaviorProfileConverterBase behaviorProfileConverterBase = BehaviorProfileConverterBase.Create();
+                System.Collections.Generic.IList<ProfileData> list = new System.Collections.Generic.List<ProfileData>();
+                foreach (IBehaviorProfileContext current in contact.BehaviorProfiles.Profiles)
+                {
+                    try
+                    {
+                        ProfileData item = behaviorProfileConverterBase.Convert(current);
+                        list.Add(item);
+                    }
+                    catch (Sitecore.Exceptions.ItemNotFoundException)
+                    {
+                        Log.Warn("Sitecore.Support.163109 - the behavior profile contains a reference to a deleted profile", this);
+                    }
+                }
+                interaction.Profiles.Initialize(list);
+
+
+                //Cannot access set porperty (internal set), use reflexion instead
+                //Commented line of original code
+                //interaction.IsContactBehaviorProfileApplied = true;
+
+                //Support code start
+                System.Reflection.PropertyInfo propertyInfo = interaction.GetType().GetProperty("IsContactBehaviorProfileApplied");
+                propertyInfo.SetValue(interaction, true, null);
+                //Support code end
+            }
+        }
+    }
+}
---
+++ src/Sitecore.Support.163109/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.163109")]
+[assembly: AssemblyProduct("Sitecore.Support.163109")]
+[assembly: ComVisible(false)]
